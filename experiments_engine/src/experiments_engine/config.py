9a561b3677f195ce569034a7261f6062f568e3af
rruuffuuss <mail@rufusponniah.com>
2026-05-15 15:57:13 +0100

    GROUPE-32 document existing MQ client code

--- experiments_engine/src/experiments_engine/config.py
+++ experiments_engine/src/experiments_engine/config.py
@@ -1,6 +1,9 @@
 import os
 
 
+# this class creates the settings object used in worker
+# settings are read from environment variables e.g. the "QUEUE_NAME" envirnoment variable
+# if the environment variable isn't set, a default value is given
 class Settings:
     experiment_queue:str = os.getenv('QUEUE_NAME', 'experiment_queue')
     queue_container:str = os.getenv('QUEUE_CONTAINER', 'queue')
