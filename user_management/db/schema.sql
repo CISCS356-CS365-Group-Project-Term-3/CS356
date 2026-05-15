fad700ba5e2afd3337f00b7b7ae6c3c10ecc52c8
Mubeen FTW <mubeen.khan.2023@uni.strath.ac.uk>
2026-05-15 01:12:59 +0100

    feature: Users Table Schema - GROUPH-21

---
+++ user_management/db/schema.sql
@@ -0,0 +1,7 @@
+CREATE TABLE user (
+    id            SERIAL PRIMARY KEY,
+    email         VARCHAR(255) NOT NULL UNIQUE,
+    username      VARCHAR(100) UNIQUE,
+    password_hash VARCHAR(255) NOT NULL,
+    role          VARCHAR(50) NOT NULL DEFAULT 'user'
+);
