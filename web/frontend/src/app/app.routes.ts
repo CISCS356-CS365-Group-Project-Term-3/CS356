567485fd19e46e6d6897c2012849d86397d25af0
sikemiO <oluwasikemi.odusanya.2023@uni.strath.ac.uk>
2026-05-15 23:40:46 +0100

    GROUPH-20: adds login page

--- web/frontend/src/app/app.routes.ts
+++ web/frontend/src/app/app.routes.ts
@@ -1,8 +1,6 @@
 import { Routes } from '@angular/router';
-import { NewExperiment } from './features/experiments/new-experiment/new-experiment';
-import { SignUp} from './features/user_management/sign-up/sign-up';
+import {Login} from './features/user_management/login/login';
 
 export const routes: Routes = [
-  { path: 'experiments/new', component: NewExperiment },
-  { path: 'sign-up', component: SignUp }
+  { path: 'login', component: Login },
 ];
---
+++ web/frontend/src/app/features/user_management/login/login.ts
@@ -0,0 +1,87 @@
+import { Component, signal} from '@angular/core';
+import {takeUntilDestroyed} from '@angular/core/rxjs-interop';
+import {FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators} from '@angular/forms';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {merge} from 'rxjs';
+import {MatRadioModule} from '@angular/material/radio';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+
+/** @title Login page */
+@Component({
+  selector: 'app-login',
+  imports: [
+    MatFormFieldModule,
+    MatInputModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatRadioModule,
+    MatButtonModule,
+    MatIconModule
+  ],
+  templateUrl: 'login.html',
+  styleUrl: 'login.scss',
+  standalone: true
+})
+
+export class Login {
+
+
+// set up login form
+  readonly loginForm = new FormGroup({
+    username: new FormControl('', Validators.required),
+    password: new FormControl('', Validators.required),
+  });
+
+  usernameErrorMessage = signal('');
+  passwordErrorMessage = signal('');
+  hide = signal(true);
+
+  constructor() {
+    merge(
+      this.loginForm.valueChanges,
+      this.loginForm.statusChanges
+    )
+      .pipe(takeUntilDestroyed())
+      .subscribe(() => {
+        this.updateUsernameErrorMessage();
+        this.updatePasswordErrorMessage();
+      });
+  }
+
+  updateUsernameErrorMessage() {
+    if (this.loginForm.get('username')?.hasError('required')) {
+      this.usernameErrorMessage.set('You must enter a value');
+    } else {
+      this.usernameErrorMessage.set('');
+    }
+  }
+  updatePasswordErrorMessage() {
+    if (this.loginForm.get('password')?.hasError('required')) {
+      this.passwordErrorMessage.set('You must enter a value');
+    } else {
+      this.passwordErrorMessage.set('');
+    }
+  }
+
+  clickPasswordEvent(event: MouseEvent) {
+    this.hide.set(!this.hide());
+    event.stopPropagation();
+  }
+
+
+  submitDetails(){
+
+    // const username = this.loginForm.get('username');
+    // const password = this.loginForm.get('password');
+
+    // submit details to API endpoint
+
+    // return error if username does not exist
+
+    // return error if password is incorrect
+
+
+  }
+}
