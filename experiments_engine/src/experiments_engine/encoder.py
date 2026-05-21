246d560ef1b718495bb11e899574252e26821715
rruuffuuss <mail@rufusponniah.com>
2026-05-21 10:10:28 +0100

    GROUPE-35 implement basic ffmpeg command methods

--- experiments_engine/src/experiments_engine/encoder.py
+++ experiments_engine/src/experiments_engine/encoder.py
@@ -1,17 +1,24 @@
+import subprocess
+
 # builds and runs ffmpeg commands.
 
 
 class Encoder:
 
-    def build_command(self, sequence, config):
-
+    def build_command(self, sequence, config) -> list[str]:
         # builds ffmeg command from decoded sequence/config
 
-        pass
+        (encoder, input_file, output_file) = self._get_encoder(sequence=sequence)
+
+        command = ['ffmpeg', '-i', input_file, '-map', '0:v', '-c', encoder, output_file]
+
+        return command
 
     def run(self, command):
 
         # Runs the ffmpeg command
+
+        subprocess.run(command)
 
         pass
 
@@ -20,3 +27,8 @@ class Encoder:
         # Checks encoding output and determines success/failure
 
         pass
+
+    @classmethod
+    def _get_encoder(cls, sequence) -> tuple[str, str, str]:
+        # hardcoded method until config is available
+        return sequence
