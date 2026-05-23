2181c2870d46fe0f90762a26bab182e40cf8c1fc
sikemiO <oluwasikemi.odusanya.2023@uni.strath.ac.uk>
2026-05-23 13:38:42 +0100

    GROUPH-18: adds home page

---
+++ web/frontend/src/app/features/user_management/home/home.ts
@@ -0,0 +1,14 @@
+import { Component} from '@angular/core';
+
+/** @title Home page */
+@Component({
+  selector: 'app-home',
+  imports: [
+  ],
+  templateUrl: 'home.html',
+  styleUrl: 'home.scss',
+  standalone: true
+})
+
+export class Home {
+}
