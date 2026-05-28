99aec90b86fb2c0a684117a6388cdf250de9d5a1
abdurshavingfun <abdurahsan06@gmail.com>
2026-05-28 14:50:44 +0100

    Adding an initial GET endpoint for testing

--- experiment_management/routes/experiments.py
+++ experiment_management/routes/experiments.py
@@ -1,5 +1,7 @@
 from flask import Flask, request
 from services import experiment_service
+from storage import experiment_store
+
 app = Flask(__name__)
 
 @app.route("/experiments", methods=["POST"])
@@ -10,6 +12,12 @@ def create_experiment_endpoint():
 
     return "", 204
 
+@app.route("/experiments", methods=["GET"])
+def get_experiments():
+    user_id = int(request.args.get("user_id"))
+    # Ideally we dont want this function to live with the DB logic but should be in the service layer
+    # But I am just going with it for now to test
+    return experiment_store.get_all_by_user(user_id)
 
 if __name__ == "__main__":
     app.run(debug=True)
