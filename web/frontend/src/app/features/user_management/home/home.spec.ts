39fc96701c95c57d0647f927bea7013dc4587517
sikemiO <oluwasikemi.odusanya.2023@uni.strath.ac.uk>
2026-05-29 11:03:31 +0100

    GROUPH-40: adds landing page and tests

--- web/frontend/src/app/features/user_management/home/home.spec.ts
+++ web/frontend/src/app/features/user_management/home/home.spec.ts
@@ -1,5 +1,7 @@
 import {ComponentFixture, TestBed} from '@angular/core/testing';
 import { Home} from "./home";
+import { RouterLink} from '@angular/router';
+import { provideRouter } from '@angular/router';
  import { expect, describe, it, beforeEach } from "vitest";
 
 
@@ -9,10 +11,11 @@ describe('Home', () => {
 
     beforeEach(async () => {
         await TestBed.configureTestingModule({
-            imports: [Home],
+            imports: [Home, RouterLink],
+          providers: [
+            provideRouter([])
+          ]
         }).compileComponents();
-
-
         fixture = TestBed.createComponent(Home);
         component = fixture.componentInstance;
 
