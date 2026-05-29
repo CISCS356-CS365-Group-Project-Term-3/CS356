8b05cd50d7a79b06102d87de31c39f4dfed477d1
abdurshavingfun <abdurahsan06@gmail.com>
2026-05-29 12:38:25 +0100

    Implementing message queue configuration

--- experiment_management/queue_setup/publisher.py
+++ experiment_management/queue_setup/publisher.py
@@ -1,3 +1,26 @@
-""" Add RabbitMQ Publisher Logic here for now creating a print message to test functionality """
+import json
+import pika
+
 def publish_to_queue(message):
+    """
+    Add RabbitMQ Publisher Logic here for now creating a print message
+    to test functionality
+    """
+   # connection = pika.BlockingConnection(
+   #     pika.ConnectionParameters(host="queue")
+   # )
+
+   # channel = connection.channel()
+
+   # channel.queue_declare(queue="experiment_queue", durable=True)
+
+   # channel.basic_publish(
+   #     exchange="",
+   #     routing_key="experiment_queue",
+   #     body=json.dumps(message),
+   #     properties=pika.BasicProperties(
+   #         delivery_mode=2
+   #     )
+   # )
+    # connection.close()
     print("EVENT:", message)
