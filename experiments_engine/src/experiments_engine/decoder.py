ede927f8f24a730f39e5aafaf0d82ac9618117be
shivvvvvv23984 <shivalishah17@gmail.com>
2026-06-04 14:57:18 +0100

    GROUPE-56- decoder child class creation

---
+++ experiments_engine/src/experiments_engine/decoder.py
@@ -0,0 +1,11 @@
+from coder import Coder
+
+# child class of Coder. builds ffmpeg decoding commands.
+
+class Decoder(Coder):
+
+    def build_command(self, sequence, config):
+
+        # building ffmpeg decoding command
+
+        pass
