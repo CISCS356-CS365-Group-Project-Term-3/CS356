0da8067b2012cefc8cdfeb1c786615b676953cf7
Fraser Nixon (Student) <fraser.nixon.2023@uni.strath.ac.uk>
2026-06-06 12:21:08 +0100

    Added first CRUD endpoints

--- infra_management/infra_management.py
+++ infra_management/infra_management.py
@@ -1,5 +1,5 @@
 from flask import Flask, request
-from models.api_models import UiOptions
+from models.api_models import *
 from models.sql_models import *
 from util.engine import get_engine, Base, query_result_as_list
 from util.data_init import data_init
@@ -10,7 +10,7 @@ app = Flask(__name__)
 engine = None
 
 @app.route("/rest/get_ui_options", methods=["GET"])
-def get_ui_options() -> UiOptions:
+def get_ui_options():
     global engine
     with Session(engine) as session:
         project_types = query_result_as_list(session.query(ProjectType).all())
@@ -36,12 +36,117 @@ def get_ui_options() -> UiOptions:
             "sequences": sequences
         }
 
-        # try:
-        #     UiOptions.model_dump(output)
-        # except Exception as e:
-        #     return f"Error occured.: {e}"
+@app.route("/rest/encoder_types", methods=["GET", "POST", "DELETE"])
+def project_types():
+    if request.method == "GET":
+        with Session(engine) as session:
+            project_types = query_result_as_list(session.query(EncoderType).all())
+            return project_types
         
-    return output
+    elif request.method == "POST":
+        if not PostProjectType.model_validate(request.json):
+            print("Json not in correct form")
+            return {"status": False, "message": "Error, request malformed."}
+        print("Got a POST request")
+        body = request.json
+        if body.get("id"): # this mean that it is not a new project type
+            id = body.get("id")
+            with Session(engine) as session:
+                if id:
+                    rows = session.query(ProjectType).filter(ProjectType.id == id).all()
+                    if len(rows) == 1:
+                        row = rows[0]
+                        row.name = body.get("name") if body.get("name") else row.name
+                        session.commit()
+                        return {"status": True, "message": "row successfully updated."}
+                    else:
+                        print("id not found")
+                        {"status": False, "message": "Error, no rows matching ID"}
+        else:
+            print("No ID found, creating new row.")
+            with Session(engine) as session:
+                new_row = ProjectType(
+                    name = body.get("name")
+                )
+                session.add(new_row)
+                session.commit()
+                return {"status": True, "message": "row successfully created."}
+
+    elif request.method == "DELETE":
+        body = request.json
+        if not DeleteProjectType.model_validate(body):
+            print("JSON not in correct format")
+            return {"status": False, "message": "Error, request malformed."}
+        id = body.get("id")
+        with Session(engine) as session:
+            rows = session.query(ProjectType).filter(ProjectType.id == id).all()
+            if len(rows) == 0:
+                print("No rows found")
+                return {"status": False, "message": "Error, no rows matching ID"}
+            elif len(rows) > 1:
+                print("Error: Multiple rows found.")
+                {"status": False, "message": "Error, multiple rows found."}
+            else:
+                row = rows[0]
+                session.delete(row)
+                session.commit()
+                return {"status": True, "message": "row successfully deleted."}
+
+@app.route("/rest/encoder_types", methods=["GET", "POST", "DELETE"])
+def encoder_types():
+    if request.method == "GET":
+        with Session(engine) as session:
+            encoder_types = query_result_as_list(session.query(EncoderType).all())
+            return encoder_types
+        
+    elif request.method == "POST":
+        if not PostProjectType.model_validate(request.json):
+            print("Json not in correct form")
+            return {"status": False, "message": "Error, request malformed."}
+        print("Got a POST request")
+        body = request.json
+        if body.get("id"): # this mean that it is not a new project type
+            id = body.get("id")
+            with Session(engine) as session:
+                if id:
+                    rows = session.query(EncoderType).filter(EncoderType.id == id).all()
+                    if len(rows) == 1:
+                        row = rows[0]
+                        row.name = body.get("name") if body.get("name") else row.name
+                        session.commit()
+                        return {"status": True, "message": "row successfully updated."}
+                    else:
+                        print("id not found")
+                        {"status": False, "message": "Error, no rows matching ID"}
+        else:
+            print("No ID found, creating new row.")
+            with Session(engine) as session:
+                new_row = EncoderType(
+                    name = body.get("name")
+                )
+                session.add(new_row)
+                session.commit()
+                return {"status": True, "message": "row successfully created."}
+
+    elif request.method == "DELETE":
+        body = request.json
+        if not DeleteProjectType.model_validate(body):
+            print("JSON not in correct format")
+            return {"status": False, "message": "Error, request malformed."}
+        id = body.get("id")
+        with Session(engine) as session:
+            rows = session.query(EncoderType).filter(EncoderType.id == id).all()
+            if len(rows) == 0:
+                print("No rows found")
+                return {"status": False, "message": "Error, no rows matching ID"}
+            elif len(rows) > 1:
+                print("Error: Multiple rows found.")
+                {"status": False, "message": "Error, multiple rows found."}
+            else:
+                row = rows[0]
+                session.delete(row)
+                session.commit()
+                return {"status": True, "message": "row successfully deleted."}
 
 def main():
     global engine
--- infra_management/models/api_models.py
+++ infra_management/models/api_models.py
@@ -1,52 +1,64 @@
 from pydantic import BaseModel, PositiveInt
-from typing import List
+from typing import List, Union
 
-class ProjectType(BaseModel):
+# class ProjectType(BaseModel):
+#     id: int
+#     name: str
+
+# class EncoderType(BaseModel):
+#     id: int
+#     name: str
+
+# class Codec(BaseModel):
+#     id: int
+#     name: str
+
+# class EncoderMode(BaseModel):
+#     id: int
+#     name: str
+
+# class VideoFile(BaseModel):
+#     id: int
+#     name: str
+#     spacial: List[PositiveInt]
+#     temporal: PositiveInt
+#     depth: PositiveInt
+#     filepath: str
+#     quality: str
+#     gamut: str
+
+# class Sequences(BaseModel):
+#     id: int
+#     name: str
+#     description: str
+#     video_files: List[VideoFile]
+
+# class Topology(BaseModel):
+#     id: int
+#     name: str
+
+# class TransmissionCondition(BaseModel):
+#     id: int
+#     name: str
+#     lower_bound: str
+#     upper_bound: str
+
+# class UiOptions(BaseModel):
+#     project_types: List[ProjectType]
+#     encoder_type: List[EncoderType]
+#     codecs: List[Codec]
+#     encoder_modes: List[EncoderMode]
+#     sequences: List[VideoFile]
+
+class CreateProjectType(BaseModel):
+    name: str
+
+class DeleteProjectType(BaseModel):
+    id: int
+
+class UpdateProjectType(BaseModel):
     id: int
     name: str
 
-class EncoderType(BaseModel):
-    id: int
-    name: str
-
-class Codec(BaseModel):
-    id: int
-    name: str
-
-class EncoderMode(BaseModel):
-    id: int
-    name: str
-
-class VideoFile(BaseModel):
-    id: int
-    name: str
-    spacial: List[PositiveInt]
-    temporal: PositiveInt
-    depth: PositiveInt
-    filepath: str
-    quality: str
-    gamut: str
-
-class Sequences(BaseModel):
-    id: int
-    name: str
-    description: str
-    video_files: List[VideoFile]
-
-class Topology(BaseModel):
-    id: int
-    name: str
-
-class TransmissionCondition(BaseModel):
-    id: int
-    name: str
-    lower_bound: str
-    upper_bound: str
-
-class UiOptions(BaseModel):
-    project_types: List[ProjectType]
-    encoder_type: List[EncoderType]
-    codecs: List[Codec]
-    encoder_modes: List[EncoderMode]
-    sequences: List[VideoFile]
-
+class PostProjectType(BaseModel):
+    root: Union[UpdateProjectType, CreateProjectType]
