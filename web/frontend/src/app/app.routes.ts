df6e634dc476d6e51d28985a444d0a2848518051
sikemiO <oluwasikemi.odusanya.2023@uni.strath.ac.uk>
2026-06-07 00:59:20 +0100

    GROUPH-78: adds forgot password and reset password components

--- web/frontend/src/app/app.routes.ts
+++ web/frontend/src/app/app.routes.ts
@@ -3,11 +3,15 @@ import { Login } from './features/user_management/login/login';
 import { SignUp } from './features/user_management/sign-up/sign-up';
 import { Home } from './features/user_management/home/home';
 import { LandingPage } from './features/user_management/landing-page/landing-page';
+import { ResetPassword } from './features/user_management/password-reset/reset-password';
+import { ForgotPassword} from './features/user_management/forgot-password/forgot-password';
 
 export const routes: Routes = [
   { path: 'login', component: Login },
   { path: 'sign-up', component: SignUp },
   { path: 'home', component: Home },
   { path: 'landing-page', component: LandingPage},
+  { path: 'reset-password', component: ResetPassword},
+  { path: 'forgot-password', component: ForgotPassword},
   { path: '', component: Home }
 ];
