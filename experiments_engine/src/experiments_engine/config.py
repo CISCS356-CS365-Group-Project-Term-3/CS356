cf4d767ae35e958c9064fa5619f8c7d41de3cbe3
rruuffuuss <mail@rufusponniah.com>
2026-06-16 00:37:57 +0100

    GROUPE-18: load experiment settings from config map

--- experiments_engine/src/experiments_engine/config.py
+++ experiments_engine/src/experiments_engine/config.py
@@ -5,5 +5,21 @@ import os
 # settings are read from environment variables e.g. the "QUEUE_NAME" envirnoment variable
 # if the environment variable isn't set, a default value is given
 class Settings:
+
+    config_map_path:str = os.getenv('CONFIG_MAP_PATH', './config_map.json')
+
     experiment_queue:str = os.getenv('QUEUE_NAME', 'experiment_queue')
-    queue_container:str = os.getenv('QUEUE_CONTAINER', 'queue')
+    queue_address:str = os.getenv('QUEUE_CONTAINER', 'localhost')
+
+    output_mongo_uri:str = os.getenv('OUTPUT_MONGO_URI','mongodb://admin:admin@localhost:27017/')
+    output_mongo_db_name = os.getenv("OUTPUT_MONGO_DB_NAME", "experiment_storage")
+
+    # expected migration do postgres- commented for now
+    #config_mongo_uri:str = os.getenv('CONFIG_MONGO_URI','mongodb://admin:admin@localhost:27017/')
+    #config_mongo_db_name = os.getenv("CONFIG_MONGO_DB_NAME", "experiment_storage")
+
+    input_directory = os.getenv("INPUT_DIR", "./experiments_engine/tests/test_data/input")
+    temp_directory = os.getenv("TEMP_DIR", "./experiments_engine/tests/test_data/temp")
+    output_directory = os.getenv("OUTPUT_DIR", "./experiments_engine/tests/test_data/output")
+
+    log_level = os.getenv("LOG_LEVEL", "INFO")
--- experiments_engine/src/experiments_engine/config_store.py
+++ experiments_engine/src/experiments_engine/config_store.py
@@ -1,35 +1,11 @@
-# mongodb backed ConfigStore for system configuration.
-
-#Provides the ability to read/lookup and save system configuration information to the database.
-
-import os
-from pymongo import MongoClient
+from .config import Settings
+import json
 
 
 class ConfigStore:
     def __init__(self, db_connection=None):
-        # create config store backed by MongoDB
-        # if db_connection == None, connect through MONGO_URI environment variable
-        if db_connection is None:
-            uri = os.getenv(
-                "MONGO_URI",
-                "mongodb://admin:admin@localhost:27017/",
-            )
-            db_name = os.getenv("MONGO_DB_NAME", "experiment_storage")
-            self._client = MongoClient(uri)
-            self._db = self._client[db_name]
-        else:
-            self._db = db_connection
-
+        with open(Settings.config_map_path, 'r') as f:
+            self.config = json.loads(f.read())
+    
     def get_config(self):
-        # retrieve the system configuration from the database
-        result = self._db.config.find_one({"_id": "system"})
-        return result
-
-    def save_config(self, config: dict) -> None:
-        # save the system configuration to the database
-        self._db.config.replace_one(
-            {"_id": "system"},
-            {"_id": "system", **config},
-            upsert=True,
-        )
+        return self.config
