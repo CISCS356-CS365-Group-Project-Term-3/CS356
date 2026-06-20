a4ff798c851819c8def0cb8deb3a1dec05c8e694
rruuffuuss <mail@rufusponniah.com>
2026-06-20 18:08:46 +0100

    GROUPE-77 implement network simulation utility methods & package initialiser for engine integration

--- experiments_engine/src/experiments_engine/network/setup_network.py
+++ experiments_engine/src/experiments_engine/network/setup_network.py
@@ -1,10 +1,12 @@
 from .setup_namespaces import setup_namespaces
 from .util import run_except
+from typing import List
 
 import logging
 logger = logging.getLogger(__name__)
 
-from ..config import Settings
+from ..config import Settings as Config
+Settings = Config.NetworkSimSettings
 
 # setup_ip_to_ip()
 #
@@ -18,27 +20,41 @@ def setup_ip_to_ip(delay: int|None=None, jitter: int|None=None, throttle_bw=None
 
     # Set up namespaces
     setup_namespaces(Settings.namespace_1, Settings.namespace_2, Settings.veth_1, Settings.veth_2)
-    logger.info("Created namespaces")
+    # logger.info("Created namespaces")
 
     # Set IP addresses of both namespace interfaces
-    ip_string1 = f'ip netns exec {Settings.namespace_1} ip addr add {Settings.ip_1}/32 dev {Settings.veth_1}'
-    ip_string2 = f'ip netns exec {Settings.namespace_2} ip addr add {Settings.ip_2}/34 dev {Settings.veth_2}'
-    run_except(ip_string1, shell=True)
-    run_except(ip_string2, shell=True)
+    run_except(["sudo", "ip", "netns", "exec", Settings.namespace_1, "ip", "addr", "add", f"{Settings.ip_1}/24", "dev", Settings.veth_1])
+    run_except(["sudo", "ip", "netns", "exec", Settings.namespace_2, "ip", "addr", "add", f"{Settings.ip_2}/24", "dev", Settings.veth_2])
 
     # Set both interfaces to up mode
-    run_except(f"ip netns exec {Settings.namespace_1} ip link set {Settings.veth_1} up", shell=True)
-    run_except(f"ip netns exec {Settings.namespace_2} ip link set {Settings.veth_2} up", shell=True)
+    run_except(["sudo", "ip", "netns", "exec", Settings.namespace_1, "ip", "link", "set", Settings.veth_1, "up"])
+    run_except(["sudo", "ip", "netns", "exec", Settings.namespace_2, "ip", "link", "set", Settings.veth_2, "up"])
 
     behavior_command = ""
     if delay  and jitter:
-        run_except(f'ip netns exec {Settings.namespace_1} tc qdisc add dev {Settings.veth_1} root netem delay {delay}ms {jitter}ms 50%')
+        run_except(["sudo", "ip", "netns", "exec", Settings.namespace_1, "tc", "qdisc", "add", "dev", Settings.veth_1, "root", "netem", "delay", f"{delay}ms", f"{jitter}ms", "50%"])
     elif delay:
-        run_except(f'ip netns exec {Settings.namespace_1} tc qdisc add dev {Settings.veth_1} root netem delay {delay}ms')
+        run_except(["sudo", "ip", "netns", "exec", Settings.namespace_1, "tc", "qdisc", "add", "dev", Settings.veth_1, "root", "netem", "delay", f"{delay}ms"])
 
     # throttle out of scope for MVP
        # behavior_command = "ip netns exec namespace1 tc qdisc add dev virtualeth1 root tbf latency 200ms burst 32kbit rate 100kbit"
     
     
     if packet_loss:
-        run_except(f'ip netns exec {Settings.namespace_1} tc qdisc add dev {Settings.veth_1} root netem loss {packet_loss}%')
+        run_except(["sudo", "ip", "netns", "exec", Settings.namespace_1, "tc", "qdisc", "add", "dev", Settings.veth_1, "root", "netem", "loss", f"{packet_loss}%"])
+
+def namespace_command_prefix(namespace: str) -> List[str]:
+    return ['sudo', 'ip', 'netns', 'exec', namespace]
+
+def encoder_endpoint():
+    return f"{Settings.protocol}://{Settings.ip_2}:{Settings.port}"
+
+def decoder_endpoint():
+    
+    endpoint = ""
+    if Settings.protocol == "udp":
+        # udp has no end signal, so use a 2 second timeout to check for end of stream (max delay is 1 second)
+        # definitely not the cleanest long  term solution
+        return f"{Settings.protocol}://127.0.0.1:{Settings.port}?timeout=2000000"
+    elif Settings == "rtsp":
+        return f"{Settings.protocol}://{Settings.ip_1}:{Settings.port}"
--- experiments_engine/src/experiments_engine/network/teardown_network.py
+++ experiments_engine/src/experiments_engine/network/teardown_network.py
@@ -1,7 +1,14 @@
 from .util import run_except
-from ..config import Settings
+from ..config import Settings as Config
+Settings = Config.NetworkSimSettings
 
 def teardown_network():
     # Tear down namespaces
-    run_except(f"ip netns del {Settings.namespace_1}", shell=True)
-    run_except(f"ip netns del {Settings.namespace_2}", shell=True)
+    run_except(["sudo", "ip", "netns", "del", Settings.namespace_1])
+    run_except(["sudo", "ip", "netns", "del", Settings.namespace_2])
+
+#always attempt to teardown network at startup- useful for debugging, remove for deployment
+try: 
+    teardown_network()
+except:
+    pass
